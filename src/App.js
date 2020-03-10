82c0e2c9061d45bfd08eb70dd6acf6c873eb248c
Kingsley <perfectman090@gmail.com>
2020-03-10 10:42:43 +0100

    modifications

--- src/App.js
+++ src/App.js
@@ -15,24 +15,25 @@ class Contact extends Component {
         return(
            //Our form goes here
            <form id="contact-form" onSubmit={ (e) => this.formSubmit(e)}>
-              
-           <div id="form-group">
-              <label class="message-name" htmlFor="message-name">Your Name</label>
-              <input className="form-control" onChange={e => this.setState({ name: e.target.value})} name="name" type="text" placeholder="Your Name" value={this.state.name}/>
+              <br></br>
+              <br></br>
+           <div class="form-group container col-md-6">
+              <label class="name" htmlFor="name">Your Name</label>
+              <input class="form-control" onChange={e => this.setState({ name: e.target.value})} name="name" type="text" placeholder="Your Name" value={this.state.name} required/>
            </div>
 
-           <div id="form-group">
-              <label class="message-email" htmlFor="message-email">Your Email</label>
-              <input className="form-control" onChange={(e) => this.setState({ email: e.target.value})} name="email" type="email" placeholder="your@email.com" required value={this.state.email} />
+           <div class="form-group container col-md-6">
+              <label class="email" htmlFor="email">Your Email</label>
+              <input class="form-control" onChange={(e) => this.setState({ email: e.target.value})} name="email" type="email" placeholder="your@email.com" value={this.state.email} required/>
            </div>
 
-           <div id="form-group">
-              <label class="message" htmlFor="message-input">Your Message</label>
-              <textarea className="form-control" onChange={e => this.setState({ message: e.target.value})} name="message" type="text" placeholder="Please write your message here" value={this.state.message} required/>
+           <div class="form-group container col-md-6">
+              <label class="message" htmlFor="message">Your Message</label>
+              <textarea class="form-control" onChange={e => this.setState({ message: e.target.value})} name="message" type="text" placeholder="Please write your message here" value={this.state.message} required/>
            </div>  
          
-           <div className="button--container">
-               <button type="submit" className="btn btn-primary" >{ this.state.buttonText }</button>
+           <div class="button--container container col-md-6">
+               <button type="submit" class="btn btn-primary" >{ this.state.buttonText }</button>
            </div>
          </form>
         );
