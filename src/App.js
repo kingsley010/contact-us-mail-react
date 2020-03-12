7c1ebb30f4309ca3ad7d37226affcbaee4d1d5cb
Kingsley <perfectman090@gmail.com>
2020-03-12 16:43:40 +0100

    modified src/app.js

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ class Contact extends Component {
           message: this.state.message
       }
       
-      axios.post('https://contact-us-mail-node-express-mkyviwooz.now.sh/', data)
+      axios.post('https://contact-us-mail-node-express-o43qgy9wy.now.sh/api/v1', data)
       .then( res => {
           this.setState({ sent: true }, this.resetForm())
       })
